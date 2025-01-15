bc0f977fbdc42e0cda5120e09135bf4514181b99
CAIO EMANUEL SILVA <77758820+caio00702@users.noreply.github.com>
2025-01-15 11:28:50 -0300

    Ajustes para deploy no Render

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import jsonServer from "json-server";
 import cors from "cors";
-import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
